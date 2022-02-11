0fd52bec7bc6feee48bd4d31df26900c2da392aa
Annepetrosyan <anpetrosyan1998@gmail.com>
2022-02-12 00:31:09 +0400

    f

--- threading_homework_1.py
+++ threading_homework_1.py
@@ -12,27 +12,28 @@ class ImageThreading:
     def read(self):
         with open(json_file, "r") as file:
             data = json.load(file)
+            data = dict(data)
+            for key, value in data.items:
+                self.key = key
+                self.value = value
 
 
-    def download_image(self, key, value):
-        with open(f"{key}.jpeg", "wb") as photo:
+    def download_image(self):
+        with open(f"{self.key}.jpeg", "wb") as photo:
             try:
-                response = requests.get(value)
+                response = requests.get(self.value)
             except Exception as err:
                 print(f"smth happened {err}")
             if response.status_code == 200:
                 photo.write(response.content)
-                print(f"image {key} is downloaded")
+                print(f"image {self.key} is downloaded")
 
     def thread(self, a):
+        thread_list = []
         for i in range(a):
             x = threading.Thread(target= download_image)
             thread_list.append(x)
             x.start()
-
-
-
-
 
 
 
